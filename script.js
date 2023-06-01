e7dcf1533128aaf48b461b341178e732f43642ef
dhanushwaran06 <ajdhanush.jvl@gmail.com>
2023-06-01 18:52:36 +0530

    commit

--- script.js
+++ script.js
@@ -1,25 +1,60 @@
-const form = document.querySelector('#form')
+const form = document.querySelector("#form");
 
-form.addEventListener("submit",(e)=>{
-    
-    if(!validateInputs()){
-        e.preventDefault();
-    }
-    
-})
+form.addEventListener("submit", (e) => {
+  if (!validate()) {
+    e.preventDefault();
+  }
+});
 
-function validateInputs(){
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    var error = document.getElementById("error1");
-    if(email == "tracker@gmail.com" && password == "tracker"){
-        window.location.assign("dashbord.html");
-        error.textContent="";
-        return false;
-    }
-    else{
-        error.textContent="Invlaid crendiential";
-        return false;
-    }
-
+function validate() {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var error = document.getElementById("error1");
+  if (email == "tracker@gmail.com" && password == "tracker") {
+    window.location.assign("dashbord.html");
+    error.textContent = "";
+    return false;
+  } else {
+    error.textContent = "Invlaid crendiential";
+    return false;
+  }
 }
+
+//! for api fatch
+
+const pokedex = document.getElementById("displaydex");
+
+const fetchPokemon = () => {
+  const promises = [];
+  for (let i = 1; i <= 9; i++) {
+    const url = `https://randomuser.me/api/?results=10${i}`;
+    promises.push(fetch(url).then((res) => res.json()));
+  }
+  Promise.all(promises).then((results) => {
+    const pokemon = results.map((results) => ({
+      name: results.name.first,
+      email: results.email,
+      city: results.location.city,
+      pic: results.picture.large,
+    }));
+    displayPokemon(pokemon);
+  });
+};
+
+const displayPokemon = (pokemon) => {
+  console.log(pokemon);
+  const pokemonHTMLString = pokemon.map(
+    (pokeman) => `
+      <li>
+              <img src="${pokeman.picture.large}"/>
+             <h2> ${pokeman.name.first}</h2>
+              <h2>${pokeman.email}</h2>
+               <h2>${pokeman.location.city}</h2>
+             </li>
+    `,
+  );
+
+  pokedex.innerHTML = pokemonHTMLString;
+};
+
+fetchPokemon();
